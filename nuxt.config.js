1e77e05ea17d92b4c072df83c7d9c1cc7dfd74e6
maaku <code10121@gmail.com>
2021-04-09 17:28:51 -0700

    Add favicon to fix 404 errors

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ module.exports = {
 			{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
 		],
 		link          : [
-			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
 			{
 				rel  : 'stylesheet',
 				href : 'https://fonts.googleapis.com/css2?family=Titillium+Web&display=swap'
